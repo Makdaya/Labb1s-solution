8edd89bd28027a1c151dce3b67974e14cf1a421e
Makdaya <makdayacob39@gmail.com>
2026-09-17 14:38:46 +0200

    Add project files.

---
+++ Labb1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Numerics;
+
+static void lab1()
+{
+
+    Console.WriteLine("Mata in en text");
+    string text = Console.ReadLine();
+    double total = 0;
+
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (!char.IsDigit(text[i]))
+            continue;
+        for (int j = i + 1; j < text.Length; j++)
+        {
+            if (!char.IsDigit(text[j]))
+                break;
+            if (text[i] == text[j])
+            {
+                string tal = text.Substring(i, j - i + 1);
+ 
+                Console.Write(text.Substring( 0, i));
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.Write(tal);
+                Console.ResetColor();
+
+
+                Console.WriteLine(text.Substring(j+1));
+
+                total += double.Parse(tal);
+               
+                break;
+            }
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine("total = "+ total);
+}
+         
+   
+
+lab1();
